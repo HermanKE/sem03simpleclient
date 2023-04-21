e727ed4563b975960a4d48e5c9e00490abaffc8c
Herman Kjosterud Erlingsen <herman.kjosterud.erlingsen@gmail.com>
2023-04-21 15:42:19 +0000

    Ferdig Alice

--- tcp_client.go
+++ tcp_client.go
@@ -4,17 +4,19 @@ import (
 	"net"
 	"log"
 	"os"
+	"github.com/HermanKE/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.2:8081")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
-
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+ 	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,5 +26,7 @@ func main() {
 		log.Fatal(err)
 	} 
 	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	//log.Printf("Kryptert melding: %s", response)
+	dekryptertMelding := mycrypt.Krypter([]rune(response), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Reply from proxy: ", string(dekryptertMelding))
 }
